dd20036edd5d358dcd349d34ca8310adbfb891fd
Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>
2025-01-06 05:51:11 +0530

    Update Print.java
    Signed-off-by: Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>

--- src/main/java/io/mosip/print/controller/Print.java
+++ src/main/java/io/mosip/print/controller/Print.java
@@ -41,8 +41,8 @@ public class Print {
 	@PreAuthenticateContentAndVerifyIntent(secret = "${mosip.event.secret}", callback = "/v1/print/print/callback/notifyPrint", topic = "${mosip.event.topic}")
 	public ResponseEntity<String> handleSubscribeEvent(@RequestBody EventModel eventModel) throws Exception {
 		printLogger.info("event recieved from websub"+", id: {}",eventModel.getEvent().getId());
-		boolean isPrinted = printService.generateCard(eventModel);
-		printLogger.info("printing status : {} for event id: {}",isPrinted,eventModel.getEvent().getId());
+		//boolean isPrinted = printService.generateCard(eventModel);
+		printLogger.info("printing status : {} for event id: {}",false,eventModel.getEvent().getId());
 		return new ResponseEntity<>("request accepted.", HttpStatus.OK);
 	}
 
