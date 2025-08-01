f0f069c931e095658b2a4a1a8d3469e924784325
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-08-01 15:30:03 +0530

    Added code to ignore properties of json

--- src/main/java/io/mosip/print/dto/DemographicDto.java
+++ src/main/java/io/mosip/print/dto/DemographicDto.java
@@ -1,8 +1,11 @@
 package io.mosip.print.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DemographicDto {
 	private String NIN;
 	private JsonValue[] givenName;
--- src/main/java/io/mosip/print/dto/NinDetailsResponse.java
+++ src/main/java/io/mosip/print/dto/NinDetailsResponse.java
@@ -1,8 +1,11 @@
 package io.mosip.print.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NinDetailsResponse {
 	private String rid;
 	private DemographicDto demographics;
