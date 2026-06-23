9f6e5b1a2fc5ba2941f4b37da2769bde4ecf09b2
Jiveshbytes <kjivesh586@gmail.com>
2026-06-23 12:37:51 +0530

    added logger for request body

--- src/main/java/io/mosip/print/service/impl/PrintServiceImpl.java
+++ src/main/java/io/mosip/print/service/impl/PrintServiceImpl.java
@@ -415,6 +415,8 @@ public class PrintServiceImpl implements PrintService{
 					eventModel.getEvent().getData().get("credentialType").toString(), ecryptionPin,
 					eventModel.getEvent().getTransactionId(), sign, "UIN", false, eventModel, registrationId, true);
 
+			printLogger.info("Perso Request for id : {} is : {}", request.getRegId(), persoRequestDto);
+
 			// Skip sending to perso service if signature is null
 			if (persoRequestDto.getBiometrics().getSignature() == null) {
                 printLogger.warn("Skipping perso service call for registration ID: {}. Reason: Signature not present", registrationId);
