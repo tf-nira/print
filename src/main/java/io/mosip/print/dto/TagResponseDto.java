7b2b943b2d2cbed1aed0479c3751238b3b2b0760
Soumya Raj <soumya.raj@technoforte.co.in>
2026-08-17 18:03:44 +0530

    face and signature handling for CHILD/MINOR records

---
+++ src/main/java/io/mosip/print/dto/TagResponseDto.java
@@ -0,0 +1,17 @@
+package io.mosip.print.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagResponseDto implements Serializable {
+    Map<String, String> tags;
+}
